01f15b0c1f1cb9dd509737e7e2394f788cb8f597
Logg <hoangdebongtoi24@gmail.com>
2026-04-06 23:08:54 +0700

    BTH3

---
+++ BTH1/BTH3/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main()
+    {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        Boolean b = true;
+        char c = 'a';
+        String s = "CodeGym";
+        
+        Console.WriteLine("Giá trị của biến i là " +i);
+        Console.WriteLine("Giá trị của biến f là " + f);
+        Console.WriteLine("Giá trị của biến d là " + d);
+        Console.WriteLine("Giá trị của biến b là " + b);
+        Console.WriteLine("Giá trị của biến c là " + c);
+        Console.WriteLine("Giá trị của biến i là " + s);
+    }
+}
