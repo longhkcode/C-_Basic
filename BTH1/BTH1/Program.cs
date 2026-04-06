80282ce0ca56e762efeed6d24fdb99b849c60330
Logg <hoangdebongtoi24@gmail.com>
2026-04-06 22:57:42 +0700

    time

---
+++ BTH1/BTH1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        DateTime localDate = DateTime.Now;
+        Console.WriteLine(localDate);
+    }
+}
