3eee81ba94556b231e53fbc28c9809607731b60a
Logg <hoangdebongtoi24@gmail.com>
2026-04-06 23:04:24 +0700

    AreaHCN

--- BTH1/BTH2/Program.cs
+++ BTH1/BTH2/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+public class Program{
+    public static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.WriteLine("Nhap vao chieu rong : ");
+        width = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap vao chieu dai : ");
+        height = float.Parse(Console.ReadLine());
+        Console.WriteLine("Area :  " + width * height);
+    }}
