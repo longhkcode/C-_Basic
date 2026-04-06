ffd4288e14374961487954ef1cf56c6ae65ee633
Logg <hoangdebongtoi24@gmail.com>
2026-04-06 23:11:58 +0700

    BTH4

---
+++ BTH1/BTH4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+
+        string yourName = Console.ReadLine();
+
+        Console.WriteLine("Hello: " + yourName);
+    }
+}
