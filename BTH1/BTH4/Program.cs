cb67c66496d5445343a72f959869520f152252be
Logg <hoangdebongtoi24@gmail.com>
2026-04-08 23:16:45 +0700

    BTH4_BenNgoai

---
+++ BTH1/BTH4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Nhap vao x y");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine("x + y = "+ x+y);
+    }
+}
