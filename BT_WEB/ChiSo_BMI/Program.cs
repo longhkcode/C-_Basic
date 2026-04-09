39196a1b70f4ab16ac77872a9547679c79ce6934
Logg <hoangdebongtoi24@gmail.com>
2026-04-09 15:41:05 +0700

    BMI

---
+++ BT_WEB/ChiSo_BMI/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap vao chieu cao : ");
+        double height = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Nhap vao can nang : ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        
+        double bmi = weight / Math.Pow(height,2);
+        bmi = Math.Round(bmi, 1);
+        
+        Console.WriteLine("BMI : " + bmi);
+        if (bmi < 18)
+        {
+            Console.Write("Underweight");
+        }else if ( bmi < 25){
+            Console.Write("Normal");
+        }else if (bmi < 30)
+        {
+            Console.Write("Overweight");
+        }
+        else
+        {
+            Console.Write("Obese");
+        }
+    }
+}
