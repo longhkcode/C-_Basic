4b3d889cebcd33d6f9a8bf6de2c18e6bb048eabf
Logg <hoangdebongtoi24@gmail.com>
2026-04-13 16:07:17 +0700

    code_T5

---
+++ BTH1/GPTB2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nhap c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+        
+        if (a == 0) return;
+        
+        double delta = b * b - 4 * a * c;
+        
+        if (delta < 0) {
+            Console.WriteLine("PT vo nghiem");
+        }else if (delta == 0) {
+            double x = -b / (2 * a);
+            Console.WriteLine("PT co nghiem kep x = " + x);
+        }else {
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("x2 = " + x2);
+        }
+    }
+}
---
+++ BTH1/LT_CauDK/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tuoi = 33;
+    }
+}
