cf0708734d7e5c296fde09925006093619b494a1
Logg <hoangdebongtoi24@gmail.com>
2026-04-13 16:04:14 +0700

    ChanLe

---
+++ BTH1/BT1_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(String[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("{0} la so chan",a);
+            }
+            else
+            {
+                Console.WriteLine("{0} la so le",a);
+            }
+        }
+}
