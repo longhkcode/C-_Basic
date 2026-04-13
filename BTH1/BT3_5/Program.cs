3c1c0d3c5da97ad2f5f03a3455091f8c7d3c56b3
Logg <hoangdebongtoi24@gmail.com>
2026-04-13 16:05:58 +0700

    Kt_SoDuong

---
+++ BTH1/BT3_5/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 0) {
+    Console.WriteLine("0");
+}else if (n > 0) {
+    Console.WriteLine("{0} la so duong",n);
+}else {
+    Console.WriteLine("{0} la so duong", n);
+}
